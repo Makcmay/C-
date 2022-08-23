ed343a5e2594688ea1a9b47586201ca72a63f55c
MakcMay <makcmay@gmail.com>
2022-08-23 20:36:53 +0300

    add

---
+++ SEMINAR2/Program.cs
@@ -0,0 +1,19 @@
+﻿// System.Console.Write("Enter number1 : ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+int number1 = 0;
+// for (int i = 2; i <= number1; i += 2)
+// {
+//     System.Console.WriteLine(i);
+// }
+
+// for (int i = 1; i <= number1; i++)
+// {
+//     if (i % 2 == 0)
+//         System.Console.WriteLine(i + " ");
+// }
+// String str =$"hello {number1}"; // выводит в строку текст + значение переменной
+// System.Console.WriteLine(str);
+
+System.Console.WriteLine(number1++); // в случае если ++ стоит после переменной то прибалять 1 будет в следующей строек,
+// а если ++ стоит впереди переменной то добавит сразу
+System.Console.WriteLine(number1);
