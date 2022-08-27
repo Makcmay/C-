11e521df0c4e7a5112cbc39ef30ce60d24c93106
MakcMay <makcmay@gmail.com>
2022-08-27 14:09:42 +0300

    add лекция3

---
+++ examples/Example012/ex12/Program.cs
@@ -0,0 +1,26 @@
+﻿void Method1()
+{
+    System.Console.WriteLine("Автор...");
+}
+// Method1(); // вызов метода
+
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+// Method2(msg: "Text");
+
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+
+// Method21(msg: "Text", count: 4); // если аргумент именнован можно поменять местами
+Method21(count: 4, msg: "New Text");
