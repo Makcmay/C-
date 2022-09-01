e934d0052d60384b10444b28ae557f6dab62cb65
MakcMay <makcmay@gmail.com>
2022-09-01 21:52:42 +0300

    add Seminar5

---
+++ Seminar5/Program.cs
@@ -0,0 +1,86 @@
+﻿// Задача 31: Задайте массив из 12 элементов, заполненный
+// случайными числами из промежутка [-9, 9]. Найдите сумму
+// отрицательных и положительных элементов массива.
+// Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма
+// положительных чисел равна 29, сумма отрицательных равна
+// -20.
+
+string SummOfNumbers()
+{
+    int[] array = new int[12];
+
+    int sumPositive = 0;
+    int sumNegative = 0;
+    int result;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-9, 10);
+
+        result = (array[i] > 0) ? (sumPositive += array[i]) : (sumNegative += array[i]);
+    }
+
+    string output = $"You array: {String.Join(", ", array)}\nSumm of positive numbers of array is {sumPositive}\nSumm of positive numbers of array is {sumNegative}";;
+
+    return output;
+}
+
+System.Console.WriteLine(SummOfNumbers());
+
+
+// Задача 32: Напишите программу замена элементов
+// массива: положительные элементы замените на
+// соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+// Задача 33: Задайте массив. Напишите программу, которая
+// определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+int[] EnterRandomArray()
+{
+    Console.Write("Enter length of array...\t");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    int[] array = new int[number];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-9, 10);
+    }
+
+    return array;
+}
+
+void ChangeElements(int[] array)
+{
+    Console.WriteLine($"You array:       {String.Join(", ", array)}");
+    
+    for (int j = 0; j < array.Length; j++)
+    {
+        array[j] = -1 * array[j];
+    }
+
+    Console.WriteLine($"You revert array: {String.Join(", ", array)}");
+}
+
+
+bool IsInArray(int[] array)
+{
+    Console.Write("Enter a number for find in array...\t");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine($"You array: {String.Join(", ", array)}");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == number) return true;
+    }
+
+    return false;
+}
+
+ChangeElements(EnterRandomArray());
+
+Console.WriteLine(IsInArray(EnterRandomArray()));
