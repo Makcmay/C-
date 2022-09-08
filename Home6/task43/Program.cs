80c6c2a542e981418ef2bbd1f176c1482c0eff71
MakcMay <makcmay@gmail.com>
2022-09-08 16:34:33 +0300

    add task43 correct

--- Home6/task43/Program.cs
+++ Home6/task43/Program.cs
@@ -5,22 +5,22 @@
 
 
 Console.Write("Enter K1=\t");
-int k1 = Convert.ToInt32(Console.ReadLine());
+double k1 = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Enter B1=\t");
-int b1 = Convert.ToInt32(Console.ReadLine());
+double b1 = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Enter K2=\t");
-int k2 = Convert.ToInt32(Console.ReadLine());
+double k2 = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Enter B2=\t");
-int b2 = Convert.ToInt32(Console.ReadLine());
+double b2 = Convert.ToInt32(Console.ReadLine());
 
 
 if (k1 != k2)
     {
         double point1 = (b2 - b1) / (k1 - k2);
-        double point2 = k1 * ((b2 - b1) / (k1 - k2)) + b1;
+        double point2 = (k1 * ((b2 - b1) / (k1 - k2))) + b1;
 
         Console.WriteLine($"Точка перечечения (x={point1};y={point2})");
     }
